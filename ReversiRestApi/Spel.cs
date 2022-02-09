7ccc5faca6e8fd0c77a1aa093565d8a8539a4eab
Rick Klaasboer <rick@klaasboer.org>
2022-02-09 18:38:22 +0100

    fix: make tests pass

--- ReversiRestApi/Spel.cs
+++ ReversiRestApi/Spel.cs
@@ -54,7 +54,7 @@ namespace ReversieISpelImplementatie.Model
 
         public bool Afgelopen() // return true als geen van de spelers een zet kan doen
         {
-            throw new NotImplementedException(); // todo!
+            return !(IsErEenZetMogelijk(Kleur.Wit) && IsErEenZetMogelijk(Kleur.Zwart));
         }
 
         public Kleur OverwegendeKleur()
@@ -84,8 +84,28 @@ namespace ReversieISpelImplementatie.Model
 
         public void DoeZet(int rijZet, int kolomZet)
         {
-            throw
-                new NotImplementedException(); // todo: maak hierbij gebruik van de reeds in deze klassen opgenomen methoden!
+            if (!ZetMogelijk(rijZet, kolomZet))
+            {
+                throw new Exception($"Zet ({rijZet},{kolomZet}) is niet mogelijk!");
+            }
+
+            bool heeftOmgedraaid = false;
+
+            for (var i = 0; i < 8; i++)
+            {
+                var result = DraaiStenenVanTegenstanderInOpgegevenRichtingOmIndienIngesloten(
+                    rijZet,
+                    kolomZet,
+                    AandeBeurt,
+                    richting[i, 0],
+                    richting[i, 1]
+                );
+
+                heeftOmgedraaid = heeftOmgedraaid ? heeftOmgedraaid : result;
+            }
+
+            Bord[rijZet, kolomZet] = AandeBeurt;
+            WisselBeurt();
         }
 
         private static Kleur GetKleurTegenstander(Kleur kleur)
@@ -176,9 +196,13 @@ namespace ReversieISpelImplementatie.Model
                    aantalNaastGelegenStenenVanTegenstander > 0;
         }
 
-        private bool DraaiStenenVanTegenstanderInOpgegevenRichtingOmIndienIngesloten(int rijZet, int kolomZet,
+        private bool DraaiStenenVanTegenstanderInOpgegevenRichtingOmIndienIngesloten(
+            int rijZet,
+            int kolomZet,
             Kleur kleurZetter,
-            int rijRichting, int kolomRichting)
+            int rijRichting,
+            int kolomRichting
+        )
         {
             int rij, kolom;
             var kleurTegenstander = GetKleurTegenstander(kleurZetter);
